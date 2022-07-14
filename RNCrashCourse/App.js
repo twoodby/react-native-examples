af4adc94eb74bd72918ceb7e7d2f8871b531834e
Timothy Woodby <timwoodby@gmail.com>
2022-07-14 16:13:44 -0400

    hold at 1.22.11

--- RNCrashCourse/App.js
+++ RNCrashCourse/App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is nuts right</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+        <View style={styles.inputContainer } >
+          <TextInput style={styles.textInput} placeholder="Your course goal!" />
+          <Button title="Add Goal" />
+        </View>
+        <View>
+          <Text>List of goals....</Text>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    paddingTop: 70,
+    paddingHorizontal:20,
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: `#cccccc`,
+    width: '80%',
+    marginEnd: 8,
+    paddingHorizontal: 4,
+  }
+
 });
